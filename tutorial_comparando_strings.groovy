02da42a7946eb4ca8ffec662b852b30414457fea
Bianca <bibca17@hotmail.com>
2016-09-28 17:20:13 -0300

    exemplos de como comparar strings com groovy

---
+++ tutorial_comparando_strings.groovy
@@ -0,0 +1,14 @@
+// Compare String
+// Using Groovy String Equals
+
+println "Apple".equals("Apple")
+println 'Banana'.equals('Banana')
+
+println "Carrots".equals('Carrots')
+println 'Beans'.equals("Beans")
+
+def testString = "Fruits"
+    println testString.equals('Fruits')
+    println testString.equals("Fruits")
+    
+// Using Groovy String
