de041046ac27320e62dc7dc2b4193dff8663078c
Bianca <bibca17@hotmail.com>
2016-09-29 17:41:46 -0300

    formatando o conteudo

--- manipulacao_Datas.groovy
+++ manipulacao_Datas.groovy
@@ -1,17 +1,36 @@
 def data = new Date()
     println data[Calendar.WEEK_OF_YEAR]
+
+println "-" * 20
     
 String dataFormatada = new Date().format("dd/MM/yyyy")
        println dataFormatada
+
+println "-" * 20
     
 String dataHoraFormatada = new Date().format("dd/MM/yyyy HH:mm:ss")
        println dataHoraFormatada 
-       
+
+println "-" * 20
+
+def stringParaData = Date.parse('dd/MM/yyyy', '29/09/2016')
+
+println stringParaData
+
+println "-" * 20
+
+// Data para String       
 println new Date().getDateString()
+
+println "-" * 20
 
 println new Date().getTimeString()
 
+println "-" * 20
+
 println new Date().getDateTimeString()
+
+println "-" * 20
 
 def data1 = Date.parse('dd/MM/yyyy', '31/12/1980')
 def data2 = Date.parse('dd/MM/yyyy', '31/12/1970')
